570c71ee23fe4fa8b69a8c5d461bf8613e56e983
chrissyalbert <chrissy_albert@yahoo.com>
2019-06-21 16:28:45 -0700

    Update Yelp.js
    Yelp.js includes the apiKey constant

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,3 +1,4 @@
+const apiKey = 'vwxuH0w4QNvrpWJ-wnEli1dh2Yb_9fmaN6C0D51dthEOX7y442GxsmXrntbmsq5kKy643DHlGpGUspyFTocUUCVKJdcRuy2mLi93jpL7CP1ab10TvV1X1gT9EjYNXXYx';
 const Yelp = {
     search(term, location, sortBy) {
         return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
